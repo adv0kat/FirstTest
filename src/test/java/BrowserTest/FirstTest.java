addc182a89e583ffa4f365d719549e37580fabbd
Zverev Yan <zverev-yan@yandex.ru>
2021-11-14 22:14:33 +0300

    add Task3

--- src/test/java/BrowserTest/FirstTest.java
+++ src/test/java/BrowserTest/FirstTest.java
@@ -1,15 +1,19 @@
 package BrowserTest;
+
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class FirstTest {
 
-@Test
+    @Test
     public void firstTest() {
-    System.setProperty ("Webdriver.chrome.driver", "path/to/chromedriver");
-    WebDriver driver = new ChromeDriver();
-    driver.get("http://www.google.ru/");
-   // driver.quit();
-}
+        System.setProperty("Webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.ru/");
+
+        driver.quit();
+    }
 
 }
---
+++ src/test/java/BrowserTest/Task3.java
@@ -0,0 +1,27 @@
+package BrowserTest;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task3 {
+
+    @Test
+    public void loginTest() {
+        System.setProperty("Webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost/litecart/admin/");
+
+        WebElement loginInput = driver.findElement(By.xpath("//input[@name='username']"));
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@name='password']"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[@name='login']"));
+        loginInput.sendKeys("admin");
+        passwordInput.sendKeys("admin");
+        loginButton.click();
+
+        driver.quit();
+    }
+
+}
