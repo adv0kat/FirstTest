e104a19abdafdbb157a09c0a297d2b7fbe2ebe9f
Zverev Yan <zverev-yan@yandex.ru>
2021-12-01 19:44:33 +0300

    Test 12

--- src/test/java/BrowserTest/Test12.java
+++ src/test/java/BrowserTest/Test12.java
@@ -4,81 +4,29 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import sun.security.pkcs11.wrapper.Constants;
-
-import java.util.List;
-
-import static BrowserTest.SetupDriver.driverNames;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class Test12 {
-    Exception e
-    WebElement priceUsd = driver.findElement(By.xpath("//input[@name='prices[USD]']"));catch(
-    WebElement priceEur = driver.findElement(By.xpath("//input[@name='prices[EUR]']"));)
-    WebElement saveButton = driver.findElement(By.xpath("//button[@name='save']"));`
-
-
-    WebElement informationPage = driver.findElement(By.xpath("//a[@href='#tab-information']"));
-        informationPage.click();
-
-//        driver.manage().timeouts().pageLoadTimeout(10000, TimeUnit.MILLISECONDS);
-
-    WebElement manufacturerInput = driver.findElement(By.xpath("//select[@name='manufacturer_id']/option[@value='1']"));
-        manufacturerInput.click();
-
-    WebElement keyWorsd = driver.findElement(By.xpath("//input[@name='keywords']"));
-        keyWorsd.sendKeys("sadasd");
-
-    WebElement shortDescription = driver.findElement(By.xpath("//input[@name='short_description[en]']"));
-        shortDescription.sendKeys("zxczxc");
-
-    WebElement descriptionInput = driver.findElement(By.xpath("//*[@class='trumbowyg-editor']"));
-        descriptionInput.sendKeys("opiojlkjlkmk");
-
-    WebElement headTitle = driver.findElement(By.xpath("//input[@name='head_title[en]']"));
-        headTitle.sendKeys("oikkok");
-
-    WebElement metaDescription = driver.findElement(By.xpath("//input[@name='meta_description[en]']"));
-        metaDescription.sendKeys("bvcb");
-
-
-    WebElement priceInput = driver.findElement(By.xpath("//a[@href='#tab-prices']"));
-        priceInput.click();
-
-//        driver.manage().timeouts().pageLoadTimeout(10000, TimeUnit.MILLISECONDS);
-
-    WebElement purchasePriceInput = driver.findElement(By.xpath("//input[@name='purchase_price']"));
-        purchasePriceInput.clear();
-        purchasePriceInput.sendKeys("200");
-
-    WebElement purchasePrice = driver.findElement(By.xpath("//select[@name='purchase_price_currency_code']//descendant::option[3]"));
-        purchasePrice.click();
-    WebElement lastProduct = driver.findElement(By.xpath("//table[@class='dataTable']//tr[@class='row semi-transparent'][last()]"));
-        priceUsd.sendKeys("1000");
-
-    {
-        return Constants.KEYWORD_FAIL + " -- Not able to select the date" + e.getMessage();
-    }
-        priceEur.sendKeys("500");
 
     @Test
-    public void runPageTest() throws InterruptedException {
-        SetupDriver setupDriver = new SetupDriver();
-        for (String driverName : driverNames) {
-            productAddTest(setupDriver.getDriver(driverName));
-        }
+//    public void runPageTest() throws InterruptedException {
+//        SetupDriver setupDriver = new SetupDriver();
+//        for (String driverName : driverNames) {
+//            productAddTest(setupDriver.getDriver(driverName));
+//        }
 //        regisration(setupDriver.getDriver("chrome"));
-    }
-        saveButton.click();
+//    }
+
 
 //        driver.manage().timeouts().pageLoadTimeout(10000,TimeUnit.MILLISECONDS);
 
-    public void productAddTest(WebDriver driver) {
+    public void productAddTest() throws InterruptedException {
 
         String path = System.getProperty("user.dir");
 
-//        System.setProperty("Webdriver.chrome.driver", "path/to/chromedriver.exe");
-//        WebDriver driver = new ChromeDriver();
+        System.setProperty("Webdriver.chrome.driver", "path/to/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
         driver.get("http://localhost/litecart/admin/");
         Login.loginLitecart(driver);
 
@@ -94,6 +42,8 @@ public class Test12 {
         WebElement codeInput = driver.findElement(By.xpath("//input[@name='code']"));
         codeInput.click();
         codeInput.sendKeys("0123");
+
+        Thread.sleep(1000);
 
         WebElement categoriesCheckBox = driver.findElement(By.xpath("//*[@id='tab-general']/table/tbody/tr[4]/td/div/table/tbody/tr[2]/td[1]/input"));
         categoriesCheckBox.click();
@@ -112,25 +62,48 @@ public class Test12 {
         String imgpath = path + "/src/main/resources/img/krolic.jpg";
         uploadImagesInput.sendKeys(imgpath);
 
+        Thread.sleep(1000);
 
-//        WebElement dateFromInput = driver.findElement(By.xpath("//*[@id='tab-general']/table/tbody/tr[10]/td/input"));
-//
-//        WebElement dateToInput = driver.findElement(By.xpath("//*[@id='tab-general']/table/tbody/tr[11]/td/input"));
-        WebElement dateWidget = driver.findElement(By.xpath(OR.getProperty(object)));
+        WebElement dateFromInput = driver.findElement(By.xpath("//*[@id=\"tab-general\"]/table/tbody/tr[10]/td/input"));
+        dateFromInput.sendKeys("05052005");
 
-        List<WebElement> rows = dateWidget.findElements(By.tagName("tr"));
-        List<WebElement> columns = dateWidget.findElements(By.tagName("td"));
+        WebElement dateToInput = driver.findElement(By.xpath("//*[@id=\"tab-general\"]/table/tbody/tr[11]/td/input"));
+        dateToInput.sendKeys("05062006");
 
-        for (WebElement cell : columns) {
-            if (cell.getText().equals(data)) {
-                cell.findElement(By.linkText(data)).click();
-                break;
-            }
-        }
-    }
-        lastProduct.click();
+        Thread.sleep(1000);
 
+        WebElement informationPage = driver.findElement(By.xpath("//a[@href='#tab-information']"));
+        informationPage.click();
 
+        WebElement manufacturerInput = driver.findElement(By.xpath("//select[@name='manufacturer_id']/option[@value='1']"));
+        manufacturerInput.click();
+
+        WebElement keyWorsd = driver.findElement(By.xpath("//input[@name='keywords']"));
+        keyWorsd.sendKeys("sadasd");
+
+        WebElement shortDescription = driver.findElement(By.xpath("//input[@name='short_description[en]']"));
+        shortDescription.sendKeys("zxczxc");
+
+        WebElement descriptionInput = driver.findElement(By.xpath("//*[@class='trumbowyg-editor']"));
+        descriptionInput.sendKeys("opiojlkjlkmk");
+
+        WebElement headTitle = driver.findElement(By.xpath("//input[@name='head_title[en]']"));
+        headTitle.sendKeys("oikkok");
+
+        WebElement metaDescription = driver.findElement(By.xpath("//input[@name='meta_description[en]']"));
+        metaDescription.sendKeys("bvcb");
+
+        WebElement priceInput = driver.findElement(By.xpath("//a[@href='#tab-prices']"));
+        priceInput.click();
+
+        WebElement purchasePriceInput = driver.findElement(By.xpath("//input[@name='purchase_price']"));
+        purchasePriceInput.clear();
+        purchasePriceInput.sendKeys("200");
+
+        WebElement purchasePrice = driver.findElement(By.xpath("//select[@name='purchase_price_currency_code']//descendant::option[3]"));
+        purchasePrice.click();
+
+        Thread.sleep(1000);
         driver.quit();
-}
+    }
 }
